7a2e9bd9b30b289ac355368cc71ebe039ad4d35e
Ioana Mitelea <miteleaioana@gmail.com>
2019-06-23 20:18:52 +0100

    Implement wave-based search

--- src/App.js
+++ src/App.js
@@ -2,16 +2,16 @@ import React from "react";
 import "./App.css";
 // import road from "./Road.json";
 
-let road = Array(100).fill(null);
+let road = Array(400).fill(null);
 road = road.map(() => {
-  return Array(200)
+  return Array(400)
     .fill(null)
     .map(element => (Math.random() > 0.2 ? 1 : 0));
 });
 
 // console.log(road);
 const MAX_DEPTH = 10000;
-const unitSize = 6;
+const unitSize = 3;
 const carCount = 1;
 const tickDelay = 16;
 const junctionChance = 0.5;
@@ -35,7 +35,11 @@ road.forEach((row, rowIndex) => {
   });
 });
 
-const targetIndex = Math.floor(Math.random() * (validRoadCells.length - 1));
+// const targetIndex = Math.floor(
+//   validRoadCells.length * 0.95 +
+//     Math.random() * (validRoadCells.length * 0.05 - 1)
+// );
+const targetIndex = validRoadCells.length - 1;
 const targetCell = validRoadCells[targetIndex];
 const target = [targetCell.x, targetCell.y];
 window.target = target;
@@ -62,69 +66,6 @@ function directionsAreOpposite(oldDirection, newDirection) {
   return opposite;
 }
 
-function isAnotherCarInFront({ x, y, speedX, speedY }, cars) {
-  return (
-    cars.filter(currentCar => {
-      return currentCar.x === x + speedX && currentCar.y === y + speedY;
-    }).length > 0
-  );
-}
-
-function isJunction(x, y) {
-  let validDirections = [];
-  for (let directionName in directions) {
-    if (canGo(x, y, directionName)) {
-      validDirections.push(directions[directionName]);
-    }
-  }
-  return validDirections.length >= 3;
-}
-
-function chooseDirection(x, y, oldDirection) {
-  let validDirections = [];
-  // debugger;
-  for (let directionName in directions) {
-    if (
-      !directionsAreOpposite(directions[directionName], oldDirection) &&
-      canGo(x, y, directionName)
-    ) {
-      validDirections.push(directions[directionName]);
-    }
-  }
-  // const newDirectionIndex = Math.floor(Math.random() * validDirections.length);
-  let newDirectionIndex = null;
-  let minDistance = Number.MAX_SAFE_INTEGER;
-  validDirections.forEach((direction, directionIndex) => {
-    const newCellX = x + direction[0];
-    const newCellY = y + direction[1];
-    const distance = getDistance(
-      { x: target[0], y: target[1] },
-      { x: newCellX, y: newCellY }
-    );
-    if (distance < minDistance) {
-      minDistance = distance;
-      newDirectionIndex = directionIndex;
-    }
-  });
-  let newDirection = validDirections[newDirectionIndex];
-  let returnValue = {
-    newSpeedX: newDirection[0],
-    newSpeedY: newDirection[1]
-  };
-
-  if (!returnValue) {
-    debugger;
-  }
-  return returnValue;
-}
-
-function getDistance(pointA, pointB) {
-  const deltaX = pointA.x - pointB.x;
-  const deltaY = pointA.y - pointB.y;
-  const distance = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
-  return distance;
-}
-
 function canGo(x, y, direction, speedXParam, speedYParam) {
   let speedX = 0;
   let speedY = 0;
@@ -137,7 +78,7 @@ function canGo(x, y, direction, speedXParam, speedYParam) {
     speedY = speedYParam;
   }
 
-  let isOK = road[y + speedY] && road[y + speedY][x + speedX];
+  let isOK = road[y + speedY] && road[y + speedY][x + speedX] === 1;
 
   if (isOK) {
     // debugger;
@@ -150,6 +91,8 @@ function canGo(x, y, direction, speedXParam, speedYParam) {
       visitedCells[cellKey] = true;
       return [x + speedX, y + speedY];
     }
+  } else {
+    return false;
   }
 }
 
@@ -168,37 +111,79 @@ class App extends React.Component {
     this.messagePrinted = false;
     this.canContinue = true;
     this.deepestLevel = 0;
-
+    this.waves = [[{ cell: [0, 0] }]];
+    window.waves = this.waves;
     window.cars = this.state.cars;
   }
 
   componentDidMount() {
-    window.tick = this.tick;
+    // window.tick = this.tick;
     // tickInterval = setInterval(this.tick, tickDelay);
     window.cars = this.printCars;
     window.start = this.start;
-    setTimeout(this.start, 1000);
+    // setTimeout(this.start, 1000);
   }
 
   start = () => {
     this.startTime = Date.now();
-    this.next(paths);
+    // this.next(paths);
+    this.nextWave();
   };
 
-  setShortestPath = currentCell => {
-    let shortestPath = this.getParentPath([], currentCell).reverse();
-    console.log("shortestPath:", shortestPath);
-    this.setState({ shortestPath });
+  isTarget = cell => {
+    return cell[0] === target[0] && cell[1] === target[1];
   };
 
-  getParentPath = (pathSoFar, currentCell) => {
-    if (!currentCell.parent) {
-      return pathSoFar;
+  nextWave = () => {
+    // debugger;
+    const currentWave = this.waves[this.waves.length - 1];
+    // console.log("starting wave", this.waves.length);
+    // console.log(this.waves);
+    let targetFound = false;
+    const nextWave = [];
+
+    for (let i = 0; i < currentWave.length; i++) {
+      const parent = currentWave[i];
+
+      for (let directionName in directions) {
+        const newCell = canGo(parent.cell[0], parent.cell[1], directionName);
+        // debugger;
+
+        if (newCell) {
+          const newParent = {
+            cell: newCell,
+            parent: parent,
+            directionName
+          };
+
+          if (this.isTarget(newCell)) {
+            targetFound = true;
+            this.setShortestPath(newParent);
+            break;
+          } else {
+            nextWave.push(newParent);
+          }
+        }
+      }
     }
-    return this.getParentPath(
-      [...pathSoFar, currentCell.parent.cell],
-      currentCell.parent
-    );
+
+    this.waves.push(nextWave);
+    // this.setState({ waves: this.waves });
+    if (targetFound) {
+      this.endTime = Date.now();
+      this.duration = this.endTime - this.startTime;
+      // alert(`Target found in ${this.duration}ms`);
+      console.log(
+        `targetFound at level ${this.waves.length} in ${this.duration} ms`
+      );
+      // this.setState({ waves: this.waves });
+    } else {
+      if (this.waves.length % 10) {
+        setTimeout(this.nextWave, 0);
+      } else {
+        this.nextWave();
+      }
+    }
   };
 
   next = currentParent => {
@@ -213,9 +198,9 @@ class App extends React.Component {
       this.messagePrinted = true;
       this.endTime = Date.now();
       this.duration = this.endTime - this.startTime;
+      setTimeout(() => alert(`Target found in ${this.duration}ms`), 1000);
       // this.setState({ path: paths });
       this.setShortestPath(currentParent);
-      setTimeout(() => alert(`Target found in ${this.duration}ms`), 1000);
       console.log(`found at level ${currentParent.level}`);
 
       return;
@@ -236,12 +221,6 @@ class App extends React.Component {
     for (let directionName in directions) {
       const newCell = canGo(cell[0], cell[1], directionName);
       let directionIsOK = true;
-      // if (currentParent.directionName) {
-      //   directionIsOK = !directionsAreOpposite(
-      //     directions[directionName],
-      //     directions[currentParent.directionName]
-      //   );
-      // }
 
       if (newCell && directionIsOK) {
         const newBranch = {
@@ -261,10 +240,6 @@ class App extends React.Component {
       // this.setState({ path: paths });
     }
     setTimeout(() => {
-      // console.log(
-      //   "currentParent.branches.length = ",
-      //   currentParent.branches.length
-      // );
       currentParent.branches.forEach(branch => {
         if (!this.targetFound) {
           this.next(branch);
@@ -273,49 +248,66 @@ class App extends React.Component {
     }, 1);
   };
 
+  setShortestPath = currentCell => {
+    // debugger;
+    let shortestPath = this.getParentPath([], currentCell).reverse();
+    // console.log("shortestPath:", shortestPath);
+    this.setState({ shortestPath });
+  };
+
+  getParentPath = (pathSoFar, currentCell) => {
+    if (!currentCell.parent) {
+      return pathSoFar;
+    }
+    return this.getParentPath(
+      [...pathSoFar, currentCell.parent.cell],
+      currentCell.parent
+    );
+  };
+
   printCars = () => {
     console.log(this.state.cars[0]);
   };
 
-  tick = () => {
-    let newCars = this.state.cars.map(car => {
-      let newSpeedX = 0;
-      let newSpeedY = 0;
-      let canMove = true;
-      if (car.x === target[0] && car.y === target[1]) {
-        canMove = false;
-        if (tickInterval) clearInterval(tickInterval);
-        // alert("yohoo");
-      }
-      const newDirection = chooseDirection(car.x, car.y, [
-        car.speedX,
-        car.speedY
-      ]);
-      newSpeedX = newDirection.newSpeedX;
-      newSpeedY = newDirection.newSpeedY;
+  // tick = () => {
+  //   let newCars = this.state.cars.map(car => {
+  //     let newSpeedX = 0;
+  //     let newSpeedY = 0;
+  //     let canMove = true;
+  //     if (car.x === target[0] && car.y === target[1]) {
+  //       canMove = false;
+  //       if (tickInterval) clearInterval(tickInterval);
+  //       // alert("yohoo");
+  //     }
+  //     const newDirection = chooseDirection(car.x, car.y, [
+  //       car.speedX,
+  //       car.speedY
+  //     ]);
+  //     newSpeedX = newDirection.newSpeedX;
+  //     newSpeedY = newDirection.newSpeedY;
 
-      let newCarProperties = {
-        ...car,
-        speedX: newSpeedX,
-        speedY: newSpeedY
-      };
+  //     let newCarProperties = {
+  //       ...car,
+  //       speedX: newSpeedX,
+  //       speedY: newSpeedY
+  //     };
 
-      if (canMove) {
-        newCarProperties.x = newCarProperties.x + newSpeedX;
-        newCarProperties.y = newCarProperties.y + newSpeedY;
-      } else {
-        newCarProperties.x = car.x;
-        newCarProperties.y = car.y;
-      }
-      return newCarProperties;
-    });
-    let newPath = [...this.state.path, [newCars[0].x, newCars[0].y]];
+  //     if (canMove) {
+  //       newCarProperties.x = newCarProperties.x + newSpeedX;
+  //       newCarProperties.y = newCarProperties.y + newSpeedY;
+  //     } else {
+  //       newCarProperties.x = car.x;
+  //       newCarProperties.y = car.y;
+  //     }
+  //     return newCarProperties;
+  //   });
+  //   let newPath = [...this.state.path, [newCars[0].x, newCars[0].y]];
 
-    this.setState({
-      cars: newCars,
-      path: newPath
-    });
-  };
+  //   this.setState({
+  //     cars: newCars,
+  //     path: newPath
+  //   });
+  // };
 
   generateCars = () => {
     return Array(carCount)
@@ -381,15 +373,27 @@ class App extends React.Component {
     });
   };
 
+  displayMap = () => {
+    let style = {
+      left: 0,
+      top: 0,
+      height: road.length * unitSize + "px",
+      width: road[0].length * unitSize + "px",
+      backgroundColor: "#444"
+    };
+    return <div style={style} />;
+  };
+
   displayRoad = () => {
     return road.map((row, rowIndex) => {
       return row.map((isRoad, columnIndex) => {
+        if (isRoad) return null;
         let style = {
           left: columnIndex * unitSize,
           top: rowIndex * unitSize,
           width: unitSize + "px",
           height: unitSize + "px",
-          backgroundColor: isRoad ? "#444" : "#fff"
+          backgroundColor: "#eee"
         };
         return (
           <div
@@ -437,6 +441,38 @@ class App extends React.Component {
     });
   };
 
+  displayWaves = () => {
+    if (!this.state.waves) {
+      return null;
+    }
+    return this.state.waves.map((wave, waveIndex) => {
+      if (!wave) {
+        return null;
+      }
+      return wave.map((parent, cellIndex) => {
+        // console.log("cell = ", cell);
+        const wavePercentage = waveIndex / (this.state.waves.length - 1);
+        const red = wavePercentage * 150 + 100;
+        const blue = wavePercentage * 150 + 100;
+        const green = wavePercentage * 150 + 100;
+        let style = {
+          left: parent.cell[0] * unitSize,
+          top: parent.cell[1] * unitSize,
+          width: unitSize / 2 + "px",
+          height: unitSize / 2 + "px",
+          backgroundColor: `rgb(0,${green},${blue})`
+        };
+        return (
+          <div
+            className="path"
+            key={`wave-${waveIndex}-cell-${cellIndex}`}
+            style={style}
+          />
+        );
+      });
+    });
+  };
+
   displayPath = () => {
     const pathElements = this.flattenLevel(this.state.path);
     // console.log(pathElements);
@@ -464,13 +500,6 @@ class App extends React.Component {
     });
   };
 
-  // flattenLevel = currentParent => {
-  //   if (currentParent.branches) {
-  //     return currentParent.cell;
-  //   }
-  //   return;
-  // };
-
   flattenLevel = currentParent => {
     const flat = [currentParent.cell];
     if (currentParent.branches) {
@@ -482,23 +511,23 @@ class App extends React.Component {
     return flat;
   };
 
-  displayPathLevel = currentParent => {
-    let branches = null;
-    if (currentParent.branches) {
-      branches = currentParent.branches.map(branch =>
-        this.displayPathLevel(branch)
-      );
-    }
-    if (!currentParent || !currentParent.cell) {
-      return null;
-    }
-  };
+  // displayPathLevel = currentParent => {
+  //   if (currentParent.branches) {
+  //     branches = currentParent.branches.map(branch =>
+  //       this.displayPathLevel(branch)
+  //     );
+  //   }
+  //   if (!currentParent || !currentParent.cell) {
+  //     return null;
+  //   }
+  // };
 
   render() {
     return (
       <div className="game">
         {this.displayRoad()}
-        {this.displayPath()}
+        {this.displayMap()}
+        {/* {this.displayWaves()} */}
         {this.displayShortestPath()}
         {this.displayCars()}
         {this.displayTarget()}
